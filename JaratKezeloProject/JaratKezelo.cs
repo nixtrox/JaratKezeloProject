e235241c46d95b50c6f8b5d6760c300afcb8d98b
nixtrox <nixtrox.vidi@gmail.com>
2024-06-07 13:53:17 +0200

    Add project files.

---
+++ JaratKezeloProject/JaratKezelo.cs
@@ -0,0 +1,199 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace JaratKezeloProject
+{
+    public class JaratKezelo
+    {
+
+
+        internal class Jarat
+        {
+            private string jaratSzam;
+            private string kezdetiRepter;
+            private string celRepter;
+            private DateTime indulasIdopontja;
+            private int aktualisKeses;
+
+            public Jarat(string jaratszam, string kezdetiRepter, string celRepter, DateTime indulasIdopontja)
+            {
+                this.jaratSzam = jaratszam;
+                this.kezdetiRepter = kezdetiRepter;
+                this.celRepter = celRepter;
+                this.indulasIdopontja = indulasIdopontja;
+                this.aktualisKeses = 0;
+            }
+
+            public string JaratSzam { get => jaratSzam; set => jaratSzam = value; }
+            public string KezdetiRepter { get => kezdetiRepter; set => kezdetiRepter = value; }
+            public string CelRepter { get => celRepter; set => celRepter = value; }
+            public DateTime IndulasIdopontja { get => indulasIdopontja; set => indulasIdopontja = value; }
+            public int AktualisKeses { get => aktualisKeses; set => aktualisKeses = value; }
+        }
+
+
+
+        private List<Jarat> jaratok = new List<Jarat>();
+
+        public void ujJarat(string jaratSzam, string repterHonnan, string repterHova, DateTime indulas)
+        {
+
+         
+
+            if(jaratSzam == null)
+            {
+                throw new ArgumentNullException(nameof(jaratSzam));
+            }
+
+            if (repterHonnan == null)
+            {
+                throw new ArgumentNullException(nameof(repterHonnan));
+            }
+
+            if (repterHova == null)
+            {
+                throw new ArgumentNullException(nameof(repterHova));
+            }
+
+            if (jaratSzam == "")
+            {
+                throw new ArgumentNullException(nameof(jaratSzam));
+            }
+
+
+            if (repterHonnan == "")
+            {
+                throw new ArgumentNullException(nameof(repterHonnan));
+            }
+
+            if (repterHova == "")
+            {
+                throw new ArgumentNullException(nameof(repterHova));
+            }
+
+
+            int i = 0;
+            while(i < jaratok.Count && jaratok[i].JaratSzam != jaratSzam)
+            {
+                i++;
+            }
+
+            if(i < jaratok.Count)
+            {
+                throw new ArgumentException("Már létezik ilyen járatszám", nameof(jaratSzam));
+            }
+
+
+
+            Jarat j1 = new Jarat(jaratSzam, repterHonnan, repterHova, indulas);
+            jaratok.Add(j1);
+
+        }
+
+        public void keses(string jaratSzam, int keses)
+        {
+            foreach(var item in jaratok)
+            {
+                if(item.JaratSzam == jaratSzam)
+                {
+                    item.AktualisKeses += keses;
+
+
+
+                    if(item.AktualisKeses < 0)
+                    {
+                        throw new ArgumentException("NegativKesesException");
+                    }
+                }
+            }
+
+           
+        }
+
+        public DateTime mikorINdul(string jaratSzam)
+        {
+            DateTime newTime = DateTime.Now ;
+
+            if(jaratSzam == null)
+            {
+                throw new ArgumentNullException(nameof(jaratSzam));
+            }
+
+            int counter = 0;
+            foreach (var item in jaratok)
+            {
+                if (item.JaratSzam == jaratSzam)
+                {
+                    counter++;
+                }
+            }
+            if (counter == 0)
+            {
+                throw new ArgumentException("Nincs ilyen járat.", nameof(jaratSzam));
+            }
+
+
+            foreach (var item in jaratok)
+            {
+                if(item.JaratSzam == jaratSzam)
+                {
+                    newTime = item.IndulasIdopontja.Add(TimeSpan.FromMinutes(item.AktualisKeses));
+                    return newTime;
+                }
+                
+               
+            }
+
+            throw new ArgumentException("Nincs ilyen járat", nameof(jaratSzam));
+            
+        }
+
+
+        public List<string>jaratokRepuloterrol(string repter)
+        {
+            List<string> mylist = new List<string>();
+
+
+
+            if(repter == null)
+            {
+                throw new ArgumentNullException(nameof(repter));
+            }
+
+
+            int counter = 0;
+            foreach(var item in jaratok)
+            {
+                if(item.KezdetiRepter == repter)
+                {
+                   counter++;
+                }
+            }
+            if(counter == 0)
+            {
+                throw new ArgumentException("Egyik járat sem indul erről a reptérről.", nameof(repter));
+            }
+
+            if(repter == null)
+            {
+                throw new ArgumentNullException(nameof(repter));
+            }
+
+            foreach(var item in jaratok)
+            {
+                if(item.KezdetiRepter == repter)
+                {
+                    mylist.Add(item.JaratSzam);
+                }
+            }
+
+            return mylist;
+        }
+
+
+    }
+
+
+
+
+
+}
---
+++ JaratKezeloTeszt/JaratKezeloTest.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using JaratKezeloProject;
+
+namespace TestJaratKezeloProject
+{
+    internal class JaratKezeloTest
+    {
+
+        [Test]
+        public void ujJarat_HelyesAdatok()
+        {
+            JaratKezelo j = new JaratKezelo();
+            j.ujJarat("jaratSzam", "indulasRepter", "erkezesRepter", DateTime.Now);
+        }
+
+
+
+        [Test]
+        public void ujJarat_NullJaratSzammal_ArgumentNullException()
+        {
+            JaratKezelo j = new JaratKezelo();
+
+            Assert.Throws<ArgumentNullException>(() => j.ujJarat(null, "indulasRepter", "erkezesRepter", DateTime.Now));
+        }
+
+        [Test]
+        public void ujJarat_UresJaratSzammal_ArgumentNullException()
+        {
+            JaratKezelo j = new JaratKezelo();
+            Assert.Throws<ArgumentNullException>(() => j.ujJarat("", "indulasRepter", "erkezesRepter", DateTime.Now));
+        }
+
+
+        [Test]
+        public void ujJarat_NullIndulasRepter_ArgumentNullException()
+        {
+            JaratKezelo j = new JaratKezelo();
+            Assert.Throws<ArgumentNullException>(() => j.ujJarat("jaratszam", null, "erkezesRepter", DateTime.Now));
+        }
+
+        [Test]
+        public void ujJarat_UresIndulasRepter_ArgumentNullException()
+        {
+            JaratKezelo j = new JaratKezelo();
+            Assert.Throws<ArgumentNullException>(() => j.ujJarat("jaratszam", "", "erkezesRepter", DateTime.Now));
+        }
+
+
+        [Test]
+        public void ujJarat_NullErkezesRepter_ArgumentNullException()
+        {
+            JaratKezelo j = new JaratKezelo();
+            Assert.Throws<ArgumentNullException>(() => j.ujJarat("jaratszam", "indulasRepter", null, DateTime.Now));
+        }
+
+        [Test]
+        public void ujJarat_UresErkezesRepter_ArgumentNullException()
+        {
+            JaratKezelo j = new JaratKezelo();
+            Assert.Throws<ArgumentNullException>(() => j.ujJarat("jaratszam", "indulasRepter", "", DateTime.Now));
+        }
+
+        [Test]
+        public void ujJarat_LetezoJaratszam_ArgumentException()
+        {
+            JaratKezelo j = new JaratKezelo();
+
+            j.ujJarat("jaratszam", "indulasRepter", "erkezesRepter", DateTime.Now);
+
+            Assert.Throws<ArgumentException>(() => j.ujJarat("jaratszam", "indulasRepter", "erkezesRepter", DateTime.Now));
+        }
+
+
+
+        [Test]
+        public void keses_MegfeleloAdatokPozitivKeses()
+        {
+            JaratKezelo j = new JaratKezelo();
+            j.ujJarat("jaratSzam", "indulasRepter", "erkezesRepter", DateTime.Now);
+            j.keses("jaratSzam", 10);
+
+        }
+
+        [Test]
+        public void keses_MegfeleloAdatokNegativKeses()
+        {
+            JaratKezelo j = new JaratKezelo();
+            j.ujJarat("jaratSzam", "indulasRepter", "erkezesRepter", DateTime.Now);
+            j.keses("jaratSzam", 15);
+            j.keses("jaratSzam", -10);
+
+        }
+
+        [Test]
+        public void keses_MegfeleloAdatokNegativKesesSumm()
+        {
+            JaratKezelo j = new JaratKezelo();
+            j.ujJarat("jaratSzam", "indulasRepter", "erkezesRepter", DateTime.Now);
+            j.keses("jaratSzam",5);
+           
+            Assert.Throws<ArgumentException>(() => j.keses("jaratSzam", -10));
+
+        }
+
+
+
+        [Test]
+        public void mikorIndul_MegfeleloBemenet()
+        {
+            JaratKezelo j = new JaratKezelo();
+            j.ujJarat("jaratSzam", "indulasRepter", "erkezesRepter", DateTime.Now);
+            j.mikorINdul("jaratSzam");
+        }
+
+
+        [Test]
+        public void mikorIndul_NullBemenet_ArgumentNullException()
+        {
+            JaratKezelo j = new JaratKezelo();
+
+
+            Assert.Throws<ArgumentNullException>(() => j.mikorINdul(null));
+        }
+
+        [Test]
+        public void mikorIndul_NincsMegfeleloJaratszam_ArgumentException()
+        {
+            JaratKezelo j = new JaratKezelo();
+
+            j.ujJarat("jaratszam", "indulasRepter", "erkezesRepter", DateTime.Now);
+
+            Assert.Throws<ArgumentException>(() => j.mikorINdul("asdasd"));
+
+
+        }
+
+
+        [Test]
+        public void jaratokRepuloterrol_MegfeleloAdatok()
+        {
+            JaratKezelo j = new JaratKezelo();
+            j.ujJarat("jaratszam", "indulasRepter", "erkezesRepter", DateTime.Now);
+            j.jaratokRepuloterrol("indulasRepter");
+        }
+
+
+        [Test]
+        public void jaratokRepuloterrol_NullRepter_ArgumentNullException()
+        {
+            JaratKezelo j = new JaratKezelo();
+
+
+            Assert.Throws<ArgumentNullException>(() => j.jaratokRepuloterrol(null));
+        }
+
+
+        [Test]
+        public void jaratokRepuloterrol_NemLétezikAMegadottRepter_ArgumentException()
+        {
+            JaratKezelo j = new JaratKezelo();
+
+            j.ujJarat("jaratszam", "indulasRepter", "erkezesRepter", DateTime.Now);
+
+            Assert.Throws<ArgumentException>(() => j.jaratokRepuloterrol("asdasd"));
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
